db3c4fd22431adb4a42b0d4af022374f4f4fd255
LauraEllingson <lellingson@ufl.edu>
2025-02-23 15:53:51 -0500

    revert and option 2

--- server/index.js
+++ server/index.js
@@ -58,10 +58,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Serve React static files AFTER API routes
-app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 // Start Server
