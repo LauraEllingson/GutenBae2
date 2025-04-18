04cc62ec9c1e15e173df6a2c0397e802ba616e96
LauraEllingson <lellingson@ufl.edu>
2025-04-18 19:42:57 -0400

    public details page

--- client/src/pages/PublicBookDetail.jsx
+++ client/src/pages/PublicBookDetail.jsx
@@ -104,7 +104,8 @@ const PublicBookDetail = () => {
   return (
     <>
       <Nav />
-      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div className="w-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+
         <div className="text-sm sm:text-base text-gray-800 leading-relaxed max-w-4xl mx-auto">
           <h1 className="mb-2">
             {formatMultilineTitle(book.title, {
